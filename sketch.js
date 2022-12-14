8eb159f3777e03fa648810bfe3942ca673bf0f76
Pjh01 <estelle0102@naver.com>
2022-12-14 18:09:27 +0900

    octave rotation

--- sketch.js
+++ sketch.js
@@ -1,16 +1,10 @@
 var wave;
-//var button;
-//var playing = false;
-// var _x = [];
-// var _y = [];
 var sounds=[261, 293, 329, 349, 391, 440, 493, 277, 311, 369, 415, 466]
 var soundsChange= [];
 let slider;
 var val;
 var _val;
-// var rectColorX;
-// var rectColorY;
-// var rectColorZ;
+var octave;
 var w;
 var blackWidth;
 
@@ -31,11 +25,6 @@ function draw(){
   blackWidth = w-20;
   for (var i = 0; i < 7; i++) {
     var x = i * w;
-    
-    // rectColorY = floor(map(rotationY, -90, 90, 0, 255))
-    // rectColorZ = floor(map(rotationZ, 0, 360, 0, 255))
-    // fill(rectColorX, rectColorY, rectColorZ);
-  
     fill(255);
     rect(x, displayHeight/2, w-1, displayHeight-1);
   }
@@ -49,25 +38,43 @@ function draw(){
   
   val = slider.value();
   _val = map(val, 0, 100, 0, 1);
-  text(_val, 230, 25)
-  
-  octaveY = rotationY
-  text(octaveY, 300, 25)
-  if(-90<= rotationY && rotationY < -75){
-    for (var n = 0; n < sounds.length; n++){
+  text("valum: "+_val, 225, 25)
+    
+  for (var n = 0; n < sounds.length; n++){
+    if(-105<= rotationY && rotationY < -75){
       soundsChange[n] = sounds[n]/8
+      octave = 1
     }
-    text(soundsChange, 300, 15)
+    if(-75<= rotationY && rotationY < -45){
+      soundsChange[n] = sounds[n]/4
+      octave = 2
+    }
+    if(-45<= rotationY && rotationY < -15){
+      soundsChange[n] = sounds[n]/2
+      octave = 3
+    }
+    if(-15<= rotationY && rotationY < 15){
+      soundsChange[n] = sounds[n]
+      octave = 4
+    }
+    if(15<= rotationY && rotationY < 45){
+      soundsChange[n] = sounds[n]*2
+      octave = 5
+    }
+    if(45<= rotationY && rotationY < 75){
+      soundsChange[n] = sounds[n]*4
+      octave = 6
+    }
+    if(75<= rotationY && rotationY < 105){
+      soundsChange[n] = sounds[n]*8
+      octave = 7
+    }
+    if(105<= rotationY && rotationY < 135){
+      soundsChange[n] = sounds[n]*16
+      octave = 8
+    }
   }
-  
-}
-
-function mousePressed() {
-  // Map mouse to the key index
-  // var key = floor(map(mouseX, 0, width, 0, sounds.length));
-  // wave.start();
-  // wave.freq(sounds[key]);
-  // wave.amp(0.5, 1);
+  text("octave: "+octave, 290, 25)
 }
 
 function mouseReleased() {
@@ -81,33 +88,33 @@ function touchStarted(){
     if(displayHeight/2 < touches[j].y && touches[j].y < displayHeight-1 ){
       var key = floor(map(touches[j].x, 0, width, 0, 7));
       wave.start();
-      wave.freq(sounds[key]);
+      wave.freq(soundsChange[key]);
       wave.amp(_val, 0.5);
     }
     if(40 < touches[j].y && touches[j].y < displayHeight/2-40){
       if(w/2+10 < touches[j].x && touches[j].x < (w/2+10)+blackWidth){
         wave.start();
-        wave.freq(sounds[7]);
+        wave.freq(soundsChange[7]);
         wave.amp(_val, 0.5);
       }
       if((w+2*w)/2+10 < touches[j].x && touches[j].x < ((w+2*w)/2+10)+blackWidth){
         wave.start();
-        wave.freq(sounds[8]);
+        wave.freq(soundsChange[8]);
         wave.amp(_val, 0.5);
       }
       if((3*w+4*w)/2+10 < touches[j].x && touches[j].x < ((3*w+4*w)/2+10)+blackWidth){
         wave.start();
-        wave.freq(sounds[9]);
+        wave.freq(soundsChange[9]);
         wave.amp(_val, 0.5);
       }
       if((4*w+5*w)/2+10 < touches[j].x && touches[j].x < ((4*w+5*w)/2+10)+blackWidth){
         wave.start();
-        wave.freq(sounds[10]);
+        wave.freq(soundsChange[10]);
         wave.amp(_val, 0.5);
       }
       if((5*w+6*w)/2+10 < touches[j].x && touches[j].x < ((5*w+6*w)/2+10)+blackWidth){
         wave.start();
-        wave.freq(sounds[11]);
+        wave.freq(soundsChange[11]);
         wave.amp(_val, 0.5);
       }
     }

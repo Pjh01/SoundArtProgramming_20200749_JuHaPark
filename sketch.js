d89e3d6f9e3da667d760043b2081a3e714db1095
Pjh01 <estelle0102@naver.com>
2022-12-16 16:17:27 +0900

    multiple sounds

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var wave;
+var wave1, wave2, wave3, wave4, wave5, wave6, wave7, wave8, wave9, wave10, wave11, wave12;
 var sounds=[261, 293, 329, 349, 391, 440, 493, 277, 311, 369, 415, 466]
 var soundsChange= [];
 let slider;
@@ -10,9 +10,53 @@ var blackWidth;
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  wave = new p5.Oscillator();
-  wave.setType('sine');
-  wave.amp(0);
+  wave1 = new p5.Oscillator();
+  wave1.setType('sine');
+  wave1.amp(0);
+  
+  wave2 = new p5.Oscillator();
+  wave2.setType('sine');
+  wave2.amp(0);
+  
+  wave3 = new p5.Oscillator();
+  wave3.setType('sine');
+  wave3.amp(0);
+  
+  wave4 = new p5.Oscillator();
+  wave4.setType('sine');
+  wave4.amp(0);
+  
+  wave5 = new p5.Oscillator();
+  wave5.setType('sine');
+  wave5.amp(0);
+  
+  wave6 = new p5.Oscillator();
+  wave6.setType('sine');
+  wave6.amp(0);
+  
+  wave7 = new p5.Oscillator();
+  wave7.setType('sine');
+  wave7.amp(0);
+  
+  wave8 = new p5.Oscillator();
+  wave8.setType('sine');
+  wave8.amp(0);
+  
+  wave9 = new p5.Oscillator();
+  wave9.setType('sine');
+  wave9.amp(0);
+  
+  wave10 = new p5.Oscillator();
+  wave10.setType('sine');
+  wave10.amp(0);
+  
+  wave11 = new p5.Oscillator();
+  wave11.setType('sine');
+  wave11.amp(0);
+  
+  wave12 = new p5.Oscillator();
+  wave12.setType('sine');
+  wave12.amp(0);
   
   slider = createSlider(0, 100, 50);
   slider.position(10, 10);
@@ -130,7 +174,18 @@ function draw(){
 }
 
 function mouseReleased() {
-  wave.amp(0, 1);
+  wave1.amp(0, 0.5);
+  wave2.amp(0, 0.5);
+  wave3.amp(0, 0.5);
+  wave4.amp(0, 0.5);
+  wave5.amp(0, 0.5);
+  wave6.amp(0, 0.5);
+  wave7.amp(0, 0.5);
+  wave8.amp(0, 0.5);
+  wave9.amp(0, 0.5);
+  wave10.amp(0, 0.5);
+  wave11.amp(0, 0.5);
+  wave12.amp(0, 0.5);
 }
 
 function touchStarted(){
@@ -138,36 +193,67 @@ function touchStarted(){
   
   for(var j = 0; j < touches.length; j++){
     if(displayHeight/2 < touches[j].y && touches[j].y < displayHeight-1 ){
-      var key = floor(map(touches[j].x, 0, width, 0, 7));
-      wave.start();
-      wave.freq(soundsChange[key]);
-      wave.amp(_val, 0.5);
+      if(0 < touches[j].x && touches[j].x < w-1){
+        wave1.start();
+        wave1.freq(soundsChange[0]);
+        wave1.amp(_val, 0.5);
+      }
+      if(w < touches[j].x && touches[j].x < (2*w)-1){
+        wave2.start();
+        wave2.freq(soundsChange[1]);
+        wave2.amp(_val, 0.5);
+      }
+      if(2*w < touches[j].x && touches[j].x < (3*w)-1){
+        wave3.start();
+        wave3.freq(soundsChange[2]);
+        wave3.amp(_val, 0.5);
+      }
+      if(3*w < touches[j].x && touches[j].x < (4*w)-1){
+        wave4.start();
+        wave4.freq(soundsChange[3]);
+        wave4.amp(_val, 0.5);
+      }
+      if(4*w < touches[j].x && touches[j].x < (5*w)-1){
+        wave5.start();
+        wave5.freq(soundsChange[4]);
+        wave5.amp(_val, 0.5);
+      }
+      if(5*w < touches[j].x && touches[j].x < (6*w)-1){
+        wave6.start();
+        wave6.freq(soundsChange[5]);
+        wave6.amp(_val, 0.5);
+      }
+      if(6*w < touches[j].x && touches[j].x < (7*w)-1){
+        wave7.start();
+        wave7.freq(soundsChange[6]);
+        wave7.amp(_val, 0.5);
+      }
     }
     if(40 < touches[j].y && touches[j].y < displayHeight/2){
       if(w/2+10 < touches[j].x && touches[j].x < (w/2+10)+blackWidth){
-        wave.start();
-        wave.freq(soundsChange[7]);
-        wave.amp(_val, 0.5);
+        wave8.start();
+        wave8.freq(soundsChange[7]);
+        wave8.amp(_val, 0.5);
       }
       if((w+2*w)/2+10 < touches[j].x && touches[j].x < ((w+2*w)/2+10)+blackWidth){
-        wave.start();
-        wave.freq(soundsChange[8]);
-        wave.amp(_val, 0.5);
+        wave9.start();
+        wave9.freq(soundsChange[8]);
+        wave9.amp(_val, 0.5);
       }
       if((3*w+4*w)/2+10 < touches[j].x && touches[j].x < ((3*w+4*w)/2+10)+blackWidth){
-        wave.start();
-        wave.freq(soundsChange[9]);
-        wave.amp(_val, 0.5);
+        wave10.start();
+        wave10.freq(soundsChange[9]);
+        wave10.amp(_val, 0.5);
       }
       if((4*w+5*w)/2+10 < touches[j].x && touches[j].x < ((4*w+5*w)/2+10)+blackWidth){
-        wave.start();
-        wave.freq(soundsChange[10]);
-        wave.amp(_val, 0.5);
+        wave11.start();
+        wave11.freq(soundsChange[10]);
+        wave11.amp(_val, 0.5);
       }
       if((5*w+6*w)/2+10 < touches[j].x && touches[j].x < ((5*w+6*w)/2+10)+blackWidth){
-        wave.start();
-        wave.freq(soundsChange[11]);
-        wave.amp(_val, 0.5);
+        wave12.start();
+        wave12.freq(soundsChange[11]);
+        wave12.amp(_val, 0.5);
       }
     }
   }

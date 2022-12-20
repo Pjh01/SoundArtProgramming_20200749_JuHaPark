ef4b5807846b061af583e82dafce07aa74845ad8
Pjh01 <estelle0102@naver.com>
2022-12-21 00:14:47 +0900

    total

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var wave1, wave2, wave3, wave4, wave5, wave6, wave7, wave8, wave9, wave10, wave11, wave12;
-var sounds=[261, 293, 329, 349, 391, 440, 493, 277, 311, 369, 415, 466]
+var sounds=[261, 293, 329, 349, 391, 440, 493, 277, 311, 369, 415, 466];
 var soundsChange= [];
 let slider;
-var val;
-var _val;
+var vol;
+var _vol;
 var octave;
 var w;
 var blackWidth;
@@ -58,9 +58,10 @@ function setup() {
   wave12.setType('sine');
   wave12.amp(0);
   
+  //볼륨 조절 슬라이더
   slider = createSlider(0, 100, 50);
   slider.position(10, 10);
-  slider.style('width', '200px');
+  slider.style('width', '180px');
 }
 
 function draw(){
@@ -77,16 +78,17 @@ function draw(){
   
   //검은 건반
   fill(0);
-  rect(w/2+10, 40, blackWidth, displayHeight/2-40);
-  rect((w+2*w)/2+10, 40, blackWidth, displayHeight/2-40);
-  rect((3*w+4*w)/2+10, 40, blackWidth, displayHeight/2-40);
-  rect((4*w+5*w)/2+10, 40, blackWidth, displayHeight/2-40);
-  rect((5*w+6*w)/2+10, 40, blackWidth, displayHeight/2-40);
+  rect(w/2+10, 50, blackWidth, displayHeight/2-50);
+  rect((w+2*w)/2+10, 50, blackWidth, displayHeight/2-50);
+  rect((3*w+4*w)/2+10, 50, blackWidth, displayHeight/2-50);
+  rect((4*w+5*w)/2+10, 50, blackWidth, displayHeight/2-50);
+  rect((5*w+6*w)/2+10, 50, blackWidth, displayHeight/2-50);
   
-  val = slider.value();
-  _val = map(val, 0, 100, 0, 1);
+  //볼륨 조절
+  vol = slider.value();
+  _vol = map(vol, 0, 100, 0, 1);
   textSize(14);
-  text("valum: "+_val, 225, 25)
+  text("volume: "+_vol, 200, 25)
   
   //Y축 기울기에 따라 옥타브 변경
   for (var n = 0; n < sounds.length; n++){
@@ -119,84 +121,84 @@ function draw(){
       octave = 7
     }
   }
-  text("octave: "+octave, 305, 25)
+  text("octave: "+octave, 290, 25)
   
-  //건반 터치 시 색 변화
+  //건반 터치 시 색 변화 & 소리 재생
   for(var m = 0; m < touches.length; m++){
-    if(40 < touches[m].y && touches[m].y < displayHeight/2){
+    if(50 < touches[m].y && touches[m].y < displayHeight/2){
       if(w/2+10 < touches[m].x && touches[m].x < (w/2+10)+blackWidth){
-        fill(111);
-        rect(w/2+10, 40, blackWidth, displayHeight/2-40);
-        wave8.freq(soundsChange[7]);
-        wave8.amp(_val, 0.5);
+        fill(123, 104, 238);
+        rect(w/2+10, 50, blackWidth, displayHeight/2-50);
+        wave8.freq(soundsChange[7]);                                  //도#
+        wave8.amp(_vol, 0.5);
       }
       if((w+2*w)/2+10 < touches[m].x && touches[m].x < ((w+2*w)/2+10)+blackWidth){
-        fill(111);
-        rect((w+2*w)/2+10, 40, blackWidth, displayHeight/2-40);
-        wave9.freq(soundsChange[8]);
-        wave9.amp(_val, 0.5);
+        fill(106, 90, 205);
+        rect((w+2*w)/2+10, 50, blackWidth, displayHeight/2-50);
+        wave9.freq(soundsChange[8]);                                  //레#
+        wave9.amp(_vol, 0.5);
       }
       if((3*w+4*w)/2+10 < touches[m].x && touches[m].x < ((3*w+4*w)/2+10)+blackWidth){
-        fill(111);
-        rect((3*w+4*w)/2+10, 40, blackWidth, displayHeight/2-40);
-        wave10.freq(soundsChange[9]);
-        wave10.amp(_val, 0.5);
+        fill(138, 43, 225);
+        rect((3*w+4*w)/2+10, 50, blackWidth, displayHeight/2-50);
+        wave10.freq(soundsChange[9]);                                //파#
+        wave10.amp(_vol, 0.5);
       }
       if((4*w+5*w)/2+10 < touches[m].x && touches[m].x < ((4*w+5*w)/2+10)+blackWidth){
-        fill(111);
-        rect((4*w+5*w)/2+10, 40, blackWidth, displayHeight/2-40);
-        wave11.freq(soundsChange[10]);
-        wave11.amp(_val, 0.5);
+        fill(102, 51, 153);
+        rect((4*w+5*w)/2+10, 50, blackWidth, displayHeight/2-50);
+        wave11.freq(soundsChange[10]);                              //솔#
+        wave11.amp(_vol, 0.5);
       }
       if((5*w+6*w)/2+10 < touches[m].x && touches[m].x < ((5*w+6*w)/2+10)+blackWidth){
-        fill(111);
-        rect((5*w+6*w)/2+10, 40, blackWidth, displayHeight/2-40);
-        wave12.freq(soundsChange[11]);
-        wave12.amp(_val, 0.5);
+        fill(75, 0, 130);
+        rect((5*w+6*w)/2+10, 50, blackWidth, displayHeight/2-50);
+        wave12.freq(soundsChange[11]);                              //라#
+        wave12.amp(_vol, 0.5);
       }
     }
     if(displayHeight/2 < touches[m].y && touches[m].y < displayHeight-1 ){
       if(0 < touches[m].x && touches[m].x < w-1){
-        fill(111);
+        fill(176, 224, 230);
         rect(0, displayHeight/2, w-1, displayHeight-1);
-        wave1.freq(soundsChange[0]);
-        wave1.amp(_val, 0.5);
+        wave1.freq(soundsChange[0]);                        //도
+        wave1.amp(_vol, 0.5);
       }
       if(w < touches[m].x && touches[m].x < (2*w)-1){
-        fill(111);
+        fill(173, 216, 230);
         rect(w, displayHeight/2, w-1, displayHeight-1);
-        wave2.freq(soundsChange[1]);
-        wave2.amp(_val, 0.5);
+        wave2.freq(soundsChange[1]);                        //레
+        wave2.amp(_vol, 0.5);
       }
       if(2*w < touches[m].x && touches[m].x < (3*w)-1){
-        fill(111);
+        fill(135, 206, 250);
         rect(2*w, displayHeight/2, w-1, displayHeight-1);
-        wave3.freq(soundsChange[2]);
-        wave3.amp(_val, 0.5);
+        wave3.freq(soundsChange[2]);                        //미
+        wave3.amp(_vol, 0.5);
       }
       if(3*w < touches[m].x && touches[m].x < (4*w)-1){
-        fill(111);
+        fill(135, 206, 235);
         rect(3*w, displayHeight/2, w-1, displayHeight-1);
-        wave4.freq(soundsChange[3]);
-        wave4.amp(_val, 0.5);
+        wave4.freq(soundsChange[3]);                        //파
+        wave4.amp(_vol, 0.5);
       }
       if(4*w < touches[m].x && touches[m].x < (5*w)-1){
-        fill(111);
+        fill(100, 149, 237);
         rect(4*w, displayHeight/2, w-1, displayHeight-1);
-        wave5.freq(soundsChange[4]);
-        wave5.amp(_val, 0.5);
+        wave5.freq(soundsChange[4]);                        //솔
+        wave5.amp(_vol, 0.5);
       }
       if(5*w < touches[m].x && touches[m].x < (6*w)-1){
-        fill(111);
+        fill(65, 105, 225);
         rect(5*w, displayHeight/2, w-1, displayHeight-1);
-        wave6.freq(soundsChange[5]);
-        wave6.amp(_val, 0.5);
+        wave6.freq(soundsChange[5]);                        //라
+        wave6.amp(_vol, 0.5);
       }
       if(6*w < touches[m].x && touches[m].x < (7*w)-1){
-        fill(111);
+        fill(0, 0, 225);
         rect(6*w, displayHeight/2, w-1, displayHeight-1);
-        wave7.freq(soundsChange[6]);
-        wave7.amp(_val, 0.5);
+        wave7.freq(soundsChange[6]);                        //시
+        wave7.amp(_vol, 0.5);
       }
     }
   }
@@ -244,7 +246,7 @@ function touchStarted(){
         wave7.start();
       }
     }
-    if(40 < touches[j].y && touches[j].y < displayHeight/2){
+    if(50 < touches[j].y && touches[j].y < displayHeight/2){
       if(w/2+10 < touches[j].x && touches[j].x < (w/2+10)+blackWidth){                    //도#
         wave8.start();
       }
